08e9d92b9bec26abaac89310ea97def189e3e980
anoldKupara <57469308+anoldKupara@users.noreply.github.com>
2022-08-16 15:16:22 +0200

    added code to retrieve all categories

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categories = _dbContext.Categories;
+            return View(categories);
         }
     }
 }
